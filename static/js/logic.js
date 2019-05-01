53499601424d195114d712cbb1c943ca8f89cc77
sponre01 <sponre01@gmail.com>
2019-05-01 18:44:30 -0500

    basic map code written

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,16 @@
-// Create function to color countries based on ___________
-  
+// Create function to color census tracts based on ____tract number_______
+function getColor(d) {
+  return d > 9000 ? '#800026' :
+         d > 8000  ? '#BD0026' :
+         d > 6000  ? '#E31A1C' :
+         d > 4000  ? '#FC4E2A' :
+         d > 2000   ? '#FD8D3C' :
+         d > 1000   ? '#FEB24C' :
+         d > 500   ? '#FED976' :
+                    '#FFEDA0';
+}
+
+
   // Adding tile layers
   var graymap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
@@ -17,12 +28,34 @@
   
   // Creating map object
   var map = L.map("map", {
-  center: [30, 0],
-  zoom: 3,
+  center: [41.8781, -87.6298],
+  zoom: 11,
   layers: [graymap, outdoors]
   });
 
-  var link = "boundaries.geojson";
+  graymap.addTo(map);
+
+  var heatmap = new L.LayerGroup();
+
+
+  var baseMaps = {
+    Grayscale: graymap,
+    Outdoors: outdoors
+  };
+  
+  var overlays = {
+    "Heatmap": heatmap
+  };
+  
+  L
+    .control
+    .layers(baseMaps, overlays)
+    .addTo(map);
+
+
+
+
+  var link = "data/boundaries.json";
 
 // Grab GEOJSON
 d3.json(link, function(error, data) {
@@ -32,8 +65,8 @@ L.geoJson(data, {
 style: function(feature) {
   return {
     color: "white",
-    fillColor: "gray",
-    fillOpacity: 0.8,
+    fillColor: getColor(feature.properties.name10),
+    fillOpacity: 0.5,
     weight: 1.5
   };
 },
@@ -44,23 +77,18 @@ onEachFeature: function(feature, layer) {
     mouseover: function(event) {
       layer = event.target;
       layer.setStyle({
-        fillOpacity: 0.1
+        fillOpacity: 0.8
       });
     },
     mouseout: function(event) {
       layer = event.target;
       layer.setStyle({
-        fillOpacity: 0.8
+        fillOpacity: 0.5
       });
     },
   });
   // Popup info
-  layer.bindPopup("<h1>" + feature.properties.ADMIN +"<br>"+ feature.properties.total + " total medals" + "</h1> <hr> <h2>" + 
-  //feature.properties.ISO_A3 + "</h2>" + 
-  "<p><h3> Gold medals: " + feature.properties.gold + "</h3></p>" +
-  "<p><h3> Silver medals: " + feature.properties.silver +"</h3></p>" +
-  "<p><h3> Bronze medals: " + feature.properties.bronze +"</h3></p>" +
-  "<a href=\"/country/" +feature.properties.ISO_A3 +"\" class=\"button\">Go to Country page</a>");
+  layer.bindPopup("<h1>" + feature.properties.namelsad10 + "</h1>");
 
 }
 }).addTo(map)
